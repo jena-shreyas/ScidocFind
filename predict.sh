89bb73cad62f6c71db6c93a8ae1b14414de3b58d
jena-shreyas <jenashreyas@gmail.com>
2023-04-13 09:05:29 +0530

    Done, wheeeee :)

--- predict.sh
+++
@@ -1,5 +0,0 @@
-allennlp predict \
-    /path/to/model.tar.gz \
-    data/CSAbstruct/test.jsonl \
-    --include-package sequential_sentence_classification \
-    --predictor SeqClassificationPredictor
